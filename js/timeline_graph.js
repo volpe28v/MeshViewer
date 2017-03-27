afa2f73822409b20f805ff7c0902fa53a0389240
volpe28v <naoki.koda@gmail.com>
2017-03-27 23:25:39 +0900

    雨量のタイムライングラフを表示するようにした

--- js/timeline_graph.js
+++ js/timeline_graph.js
@@ -10,29 +10,24 @@ function TimelineGraph(){
     var x = self.x;
     var y = self.y;
 
-    var values = self.meshes.map(function(mesh){
-      return {
-        name: mesh.name,
-        value: Number(mesh.meshArray[y][x])
-      };
-    });
-
-    var max = d3.max(values, function(d){ return d.value;});
+    var max = d3.max(self.meshes, function(d){ return Number(d.meshArray[y][x]);});
+    var xWidth = 481 / self.meshes.length;
+    xWidth = xWidth > 20 ? 20 : xWidth;
 
     var yScale = d3.scaleLinear()
       .domain([0, max])
-      .range([0, 200]);
+      .range([2, 57]);
 
     var timeline= d3.select('svg#timeline')
-      .selectAll('rect').data(values);
+      .selectAll('rect').data(self.meshes);
     timeline.enter().append('rect');
     timeline.exit().remove();
     timeline
-      .attr("x", function(d, i){ return i * 30; })
-      .attr("y", 0)
-      .attr("width", 20)
-      .attr("height", function(d){ return yScale(d.value)})
-      .attr("fill", "deepskyblue");
+      .attr("x", function(d, i){ return i * xWidth; })
+      .attr("y", function(d){ return 56 - yScale(Number(d.meshArray[y][x]))})
+      .attr("width", xWidth - 2)
+      .attr("height", function(d){ return yScale(Number(d.meshArray[y][x]))})
+      .attr("fill", function(d){ return d == self.active ? "#dccb18" : "deepskyblue";});
   }
 
   self.updateMeshes = function(meshes){
@@ -42,7 +37,7 @@ function TimelineGraph(){
 
   self.updateActive = function(active){
     self.active = active;
-    //reflesh();
+    reflesh();
   }
 
   self.updateCoordinate = function(params){
